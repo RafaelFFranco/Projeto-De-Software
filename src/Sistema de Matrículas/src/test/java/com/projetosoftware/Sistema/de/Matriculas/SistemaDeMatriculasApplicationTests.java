d9bfd33df6331f53cdcde631c0c2e02e9a241718
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-08-18 11:20:57 -0300

    Add files via upload

---
+++ src/Sistema de Matrículas/src/test/java/com/projetosoftware/Sistema/de/Matriculas/SistemaDeMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosoftware.Sistema.de.Matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
